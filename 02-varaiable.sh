c6d96b027772b9260cfd6f8ebde210b58a35f833
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-03 18:08:28 +0530

    this is second commit

---
+++ 02-varaiable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: hellow suresh, how are you"
+echo "Suresh :: hi Ramesh ia m fine, how about you"
+echo "Ramesh :: I am fine too, how is your work"
+echo "Suresh :: not bad as much, now am thinking to migrate to devops"
