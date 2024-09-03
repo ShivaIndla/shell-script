8b83297ec9e438c80a6c6396e087c91fdc8a3f35
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-03 22:59:16 +0530

    for shell scripting

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "please enter the username::"
-read USERNAME #here username is variable
+echo "please enter the Username::"
+read -s USERNAME #here username is variable (-s means hiding the value)
 
-echo "please enter passwoord::"
-read PASSWORD
+echo "please enter Passwoord::"
+read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
