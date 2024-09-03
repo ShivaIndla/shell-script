1a46f7c88c7eec53157393c82051f4fcd613b02d
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 00:07:32 +0530

    for shell scripting

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -3,3 +3,5 @@
 echo "All variables are: $@" #(Print all possible values like *) #sh 06-special-vars.sh aws devops
 echo "No of variables passed: $#"
 echo "Script name: $0"  #(prints the script name)
+echo "Present working directory: $PWD"
+echo "Home directory of user: $HOME"
