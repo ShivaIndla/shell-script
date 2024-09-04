a97b87a3465364b1547b7d38bca9ae7ae64f5698
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-05 01:22:44 +0530

    for shell scripting

---
+++ 10-colours.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+echo "============================***========================"
+
+echo "running $0"
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+RED="\e[31m"
+GREEN="\e[32m"
+NORMAL="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+        then
+         echo -e "$2 ....is $RED fail $NORMAL"
+         exit 1
+        else
+        echo -e "$2 ...is $GREEN success $NORMAL"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+    then
+        echo "Please run this ' $0 ' with root access."
+        exit 1  # Manually exit if error comes
+    else
+        echo "You are SUPER user"
+fi    
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql" &>>$LOGFILE
+
+
+
+dnf install gitt -y
+VALIDATE $? "Installing git" &>>$LOGFILE
