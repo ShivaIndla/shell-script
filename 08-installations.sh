3c1546d1a74a457454b3a1dff04044ffd03e0fb8
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 21:56:12 +0530

    for shell scripting

--- 08-installations.sh
+++ 08-installations.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
     then
-        echo "Please run this " $0 " with root access."
+        echo "Please run this ' $0 ' with root access."
         exit 1
     else
         echo "You are SUPER user"
