a5fea44ebce92188311dc101c9b56716ad30d36c
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 19:21:41 +0530

    for shell scripting

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 10 ]  # (-gt, -lt, -eq, -ne, -ge, -le)
+if [ $NUMBER -ge 10 ]  # (-gt, -lt, -eq, -ne, -ge, -le)
 then
-    echo "Given $NUMBER is grater than 10"
+    echo "Given $NUMBER is grater than or equal to 10"
 else
 echo "Given $NUMBER is less than 10"
 fi
