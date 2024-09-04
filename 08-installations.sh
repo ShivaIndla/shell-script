582246a25d5c78a2c87cf8f65d3fe740f9a4269e
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 22:04:23 +0530

    for shell scripting

--- 08-installations.sh
+++ 08-installations.sh
@@ -5,8 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
     then
         echo "Please run this ' $0 ' with root access."
-        exit 1
+        exit 1  # Manually exit if error comes
     else
         echo "You are SUPER user"
 fi    
-dnf install mysql -y
+
+dnf install mysqll -y
+if [ $? -ne 0 ]
+    then
+        echo "installing of mysql.....is fail"
+        exit 1
+    else
+        echo "mysql is installed"
+fi
+
+dnf git install -y
