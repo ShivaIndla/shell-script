d0de3fd8fc4b03feb8c5d2475190b1bc1c784848
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 13:05:01 +0530

    for shell scripting

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -5,3 +5,6 @@ echo "No of variables passed: $#"
 echo "Script name: $0"  #(prints the script name)
 echo "Present working directory: $PWD"
 echo "Home directory of user: $HOME"
+echo "Which user is running this script is: $USER"
+echo "Host name: $HOSTNAME"
+echo "PID of current script: $$"
