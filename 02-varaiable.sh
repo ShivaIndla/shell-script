b1b4b9f40048d34b67ccb5876f411140c5832223
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 20:38:30 +0530

    for shell scripting

--- 02-varaiable.sh
+++ 02-varaiable.sh
@@ -6,4 +6,4 @@ PERSON2=$Name2
 echo "$PERSON1 :: hellow $PERSON2, how are you"
 echo "$PERSON2 :: hi $PERSON1 ia m fine, how about you"
 echo "$PERSON1 :: I am fine too $PERSON2, how is your work"
-echo "$PERSON2 :: not bad as much $PERSON1, now am thinking to migrate to devops"
+echo "$PERSON2 :: not bad as much $PERSON1, now am thinking to move to devops"
