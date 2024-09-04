02ed3269ac910669e20d0fcb1c858e80645eb3ca
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-04 19:33:19 +0530

    for shell scripting

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+echo "Script name: $0 has started"  #(prints the script name)
 echo "All variables are: $@" #(Print all possible values like *) #sh 06-special-vars.sh aws devops
 echo "No of variables passed: $#"
-echo "Script name: $0"  #(prints the script name)
 echo "Present working directory: $PWD"
 echo "Home directory of user: $HOME"
 echo "Which user is running this script is: $USER"
