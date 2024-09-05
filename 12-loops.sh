5322ac1960fba7a4a44da69ad66ef87db6b3090d
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-05 15:53:02 +0530

    for shell scripting

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,6 +5,19 @@ RED="\e[31m"
 GREEN="\e[32m"
 NORAML="\e[0m"
 
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then
+        echo " $2 is allready installed and skipping this"
+    else
+
+        echo "installing $2 package"
+        dnf install $2 -y
+    fi
+    exit 1
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script as root access"
@@ -15,5 +28,8 @@ fi
 
 for i in $@
 do
-    echo -e "installing of: $GREEN $i $NORAML is done"
+    echo -e "Package to install: $GREEN $i $NORAML"
+
+    dnf list installed $i
+    VALIDATE $? $i
 done
